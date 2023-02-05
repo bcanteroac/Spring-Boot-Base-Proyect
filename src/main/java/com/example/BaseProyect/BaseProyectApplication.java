0bdb3efc17dd7d86f102b95373cec74efd44c59a
bcanteroac <91626533+bcanteroac@users.noreply.github.com>
2023-02-05 05:13:26 -0300

    Base Proyect

---
+++ src/main/java/com/example/BaseProyect/BaseProyectApplication.java
@@ -0,0 +1,13 @@
+package com.example.BaseProyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseProyectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BaseProyect/BaseProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BaseProyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
